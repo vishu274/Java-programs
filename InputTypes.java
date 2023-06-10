e2a540beae42aa0d30ce88d98f9ada33d924cbbb
Vaishali kumari <135591440+vishu274@users.noreply.github.com>
2023-06-10 12:06:06 +0530

    Initial commit

---
+++ InputTypes.java
@@ -0,0 +1,24 @@
+/** 1) input variables of following datatypes with scanner .a) int,b)float,c)double,d)char. **/
+
+import java.util.Scanner;
+class InputTypes{
+public static void main (String  args[]){
+Scanner s = new Scanner (System.in);
+System.out.println("enter the value of datatypes");
+int a;
+System.out.println("enter the value of integer );
+a= s.nextInt();
+System.out.println("entered the value ="+ a);
+
+ 
+float b;
+b= s.nextInt();
+System.out.println("enter the value of float =" +b);
+double c;
+c= s.nextInt();
+System.out.println("enter the value of double =" +c);
+
+
+}
+
+}

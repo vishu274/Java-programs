5ce93046a881d6a14be3dca8f70c8e01f055bfc1
Vaishali kumari <135591440+vishu274@users.noreply.github.com>
2023-06-17 00:57:28 +0530

    Matrix

---
+++ Matrix.java
@@ -0,0 +1,19 @@
+//addition of 2d matrix
+public class Matrix{  
+public static void main(String args[]){  
+
+int a[][]={{1,6},{1,4}};    
+int b[][]={{9,3},{2,7}};    
+    
+   
+int c[][]=new int[2][2]; 
+    
+   
+for(int i=0;i<2;i++){    
+for(int j=0;j<2;j++){    
+c[i][j]=a[i][j]+b[i][j];     
+System.out.print(c[i][j]+" ");    
+}    
+System.out.println("matrix");   
+}    
+}}  

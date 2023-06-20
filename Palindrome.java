72e3757e77466c3a87e290f04b123ed1788c70fc
Vaishali kumari <135591440+vishu274@users.noreply.github.com>
2023-06-20 22:07:30 +0530

    create palindeome

---
+++ Palindrome.java
@@ -0,0 +1,25 @@
+ /*Write a function to check whether the entered number is palindrome or not?*/
+ public class Palindrome {
+    public static boolean isPalindrome(int number) {
+        int originalNumber = number;
+        int reversedNumber = 0;
+
+        while (number > 0) {
+            int digit = number % 10;
+            reversedNumber = reversedNumber * 10 + digit;
+            number /= 10;
+        }
+
+        return originalNumber == reversedNumber;
+    }
+
+    public static void main(String[] args) {
+        int number = 12321;
+
+        if (isPalindrome(number)) {
+            System.out.println(number + " is a palindrome.");
+        } else {
+            System.out.println(number + " is not a palindrome.");
+        }
+    }
+}
